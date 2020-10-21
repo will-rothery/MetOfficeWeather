05b9be6e5a704a8fd78aa5f1ce4fe2b34b5349c3
William Rothery <will.rothery@live.co.uk>
2020-10-21 11:21:34 +0100

    first commit

--- src/main/java/training/metofficeweather/Main.java
+++ src/main/java/training/metofficeweather/Main.java
@@ -1,9 +1,21 @@
 package training.metofficeweather;
 
+import org.glassfish.jersey.jackson.JacksonFeature;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+
 public class Main {
+
+    private String apiKey = "0f6a1d47-e487-4c1f-9e9c-6e5bb230508b";
+
     public static void main(String args[]) {
-
-
-        // Your code here!
+        Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
+        client.target("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=0f6a1d47-e487-4c1f-9e9c-6e5bb230508b")
+                .queryParam("cnt", "10")
+                .queryParam("mode", "json")
+                .queryParam("units", "metric")
+                .queryParam("appid", apiKey)
+        ;
     }
-}	
+}
